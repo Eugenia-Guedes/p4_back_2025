197ef0d457433c58553cfc49901da325e95074fc
Eugenia Rodrigues de Carvalho Vieira de Melo Guede <EugeniaGuedes@academicos.com>
2025-09-26 16:17:15 -0300

    Atualização

--- src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -13,7 +19,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +28,33 @@ public class Usuario {
     private String login;
 
     private String senha;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of
+                (new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public String getPassword() {
+        return senha;
+    }
+
+    public String getUsername() {
+        return login;
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
 }
